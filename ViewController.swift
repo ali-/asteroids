d04e23fbd832f6a2c1f7a9d66a0a98bc24e44b14
Ali <mail@ali.af>
2020-08-07 03:17:39 -0700

    Update

--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,7 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
 	var scene = SCNScene()
 	var playerLocation = 1 // [ LEFT(0), MID(1), RIGHT(2) ]
 	var playerNode = SCNNode()
+	var thrusterNode = SCNNode()
 	var sceneView = SCNView()
 	
 	override func viewDidLoad() {
@@ -33,14 +34,14 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
 
 	func createBullet() {
 		let sphere = SCNCylinder(radius: 1, height: 10)
-		let move = SCNAction.move(by: SCNVector3Make(0, 20, 0), duration: 2)
+		let move = SCNAction.move(by: SCNVector3(0, 0, -250), duration: 0.15)
 		let material = SCNMaterial()
 		material.diffuse.contents = UIImage(named: Bundle.main.path(forResource: "laser", ofType: "jpg")!)
 		sphere.materials = [material]
 		
 		let bullet = SCNNode(geometry: sphere)
 		scene.rootNode.addChildNode(bullet)
-		bullet.eulerAngles = SCNVector3Make(90, 0, 0)
+		bullet.eulerAngles = SCNVector3(90, 0, 0)
 		bullet.position = playerNode.position
 		bullet.physicsBody?.isAffectedByGravity = false
 		bullet.runAction(move, completionHandler: {
@@ -58,19 +59,24 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
 		// Camera
 		let cameraNode = SCNNode()
 		cameraNode.camera = SCNCamera()
-		cameraNode.position = SCNVector3Make(0, 30, 100)
-		//cameraNode.eulerAngles = SCNVector3Make(0, 0, 0)
+		cameraNode.position = SCNVector3(0, 30, 80)
+		cameraNode.eulerAngles = SCNVector3(25, 0, 0)
+		cameraNode.camera?.zFar = 500
 		scene.rootNode.addChildNode(cameraNode)
 		
 		// Player
 		let box = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 0)
 		playerNode = SCNNode(geometry: box)
-		playerNode.position = SCNVector3Make(0, 0, 0)
+		playerNode.position = SCNVector3(0, 0, -10)
 		scene.rootNode.addChildNode(playerNode)
+		
+		thrusterNode = scene.rootNode.childNode(withName: "thruster", recursively: true)!
+		thrusterNode.position = SCNVector3(0, 0, 0)
 		
 		// Effects
 		let particleNode = scene.rootNode.childNode(withName: "stars", recursively: true)!
-		particleNode.position = SCNVector3Make(0, 30, 75)
+		particleNode.position = SCNVector3(0, 32.5, 50)
+		particleNode.eulerAngles = SCNVector3(-cameraNode.eulerAngles.x, 0, 0)
 		
 		sceneView.allowsCameraControl = false
 		sceneView.backgroundColor = .white
@@ -87,14 +93,16 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
 			switch swipeGesture.direction {
 				case UISwipeGestureRecognizer.Direction.right:
 					if playerLocation < 2 {
-						let move = SCNAction.move(by: SCNVector3Make(15, 0, 0), duration: 0.1)
+						let move = SCNAction.move(by: SCNVector3(15, 0, 0), duration: 0.1)
 						playerNode.runAction(move)
+						thrusterNode.runAction(move)
 						playerLocation += 1
 					}
 				case UISwipeGestureRecognizer.Direction.left:
 					if playerLocation > 0 {
-						let move = SCNAction.move(by: SCNVector3Make(-15, 0, 0), duration: 0.1)
+						let move = SCNAction.move(by: SCNVector3(-15, 0, 0), duration: 0.1)
 						playerNode.runAction(move)
+						thrusterNode.runAction(move)
 						playerLocation -= 1
 					}
 				default:
