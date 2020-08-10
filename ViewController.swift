07eb4669b6af287e977d7eaf0810fc09995983e5
Ali <mail@ali.af>
2020-08-10 00:14:04 -0700

    Physics

--- ViewController.swift
+++ ViewController.swift
@@ -5,28 +5,30 @@
 import UIKit
 import SceneKit
 
-class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsContactDelegate {
+var scene: SCNScene!
+var sceneView: SCNView!
+var playerLocation = 1
+var playerNode: SCNNode!
+var thrusterNode: SCNNode!
+var score = 0
+var scoreLabel: UILabel!
+var health = 3
+var healthLabel: UILabel!
+var debugLabel: UILabel!
+var objectCount = 0
 
-	var scene: SCNScene!
-	var sceneView: SCNView!
-	var playerLocation = 1
-	var playerNode: SCNNode!
-	var thrusterNode: SCNNode!
-	var score = 0
-	var scoreLabel: UILabel!
-	var health = 3
-	var healthLabel: UILabel!
+class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsContactDelegate {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		// Scene
 		scene = SCNScene(named: "starfield.scn")!
+		scene.physicsWorld.contactDelegate = self
 		sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
 		sceneView.allowsCameraControl = false
 		sceneView.delegate = self
 		sceneView.scene = scene
-		sceneView.scene?.physicsWorld.contactDelegate = self
 		drawScene()
 		view.addSubview(sceneView)
 		
@@ -37,13 +39,9 @@ class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsCont
 		let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swipe))
 		swipeRight.direction = UISwipeGestureRecognizer.Direction.right
 		view.addGestureRecognizer(swipeRight)
-		let doubleTap = UITapGestureRecognizer(target: self, action: #selector(tap))
+		let doubleTap = UITapGestureRecognizer(target: self, action: #selector(createLaser))
 		doubleTap.numberOfTapsRequired = 2
 		view.addGestureRecognizer(doubleTap)
-	}
-	
-	func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
-		print("Contact")
 	}
 	
 	func drawScene() {
@@ -56,6 +54,9 @@ class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsCont
 		scoreLabel = UILabel(frame: CGRect(x: 20, y: 40, width: view.frame.width-40, height: 30))
 		scoreLabel.text = "Score: \(score)"
 		sceneView.addSubview(scoreLabel)
+		debugLabel = UILabel(frame: CGRect(x: 20, y: 70, width: view.frame.width-40, height: 30))
+		debugLabel.text = ""
+		sceneView.addSubview(debugLabel)
 		
 		// Camera
 		let cameraNode = SCNNode()
@@ -71,6 +72,9 @@ class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsCont
 		playerNode.name = "player"
 		playerNode.position = SCNVector3(0, 0, 0)
 		playerNode.eulerAngles = SCNVector3(55, 0, 0)
+		playerNode.physicsBody?.categoryBitMask = PhysicsObject.player.rawValue
+		playerNode.physicsBody?.collisionBitMask = PhysicsObject.asteroid.rawValue | PhysicsObject.enemy.rawValue
+		playerNode.physicsBody?.contactTestBitMask = PhysicsObject.contact.rawValue
 		thrusterNode = scene.rootNode.childNode(withName: "thruster", recursively: true)!
 		thrusterNode.position = SCNVector3(0, 0, 10)
 		scene.rootNode.addChildNode(playerNode)
@@ -80,15 +84,9 @@ class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsCont
 		particleNode.position = SCNVector3(0, 50, 50)
 		particleNode.eulerAngles = SCNVector3(-cameraNode.eulerAngles.x, 0, 0)
 		
-		// Enemies
-		let box = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 0)
-		let enemyNode = SCNNode(geometry: box)
-		enemyNode.position = SCNVector3(0, 0, -300)
-		enemyNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: enemyNode))
-		enemyNode.physicsBody?.categoryBitMask = PhysicsObject.enemy.rawValue
-		enemyNode.physicsBody?.isAffectedByGravity = false
-		enemyNode.name = "enemy"
-		scene.rootNode.addChildNode(enemyNode)
+		createObject(-1)
+		createObject(0)
+		createObject(1)
 	}
 	
 	func updateHealth() {
@@ -100,13 +98,43 @@ class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsCont
 		}
 	}
 	
-	func updateScore() {
-		scoreLabel.text = "Score: \(score)"
+	func createObject(_ position: Int?) {
+		// Enemies
+		let box = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 0)
+		let move = SCNAction.move(by: SCNVector3(0, 0, (800+(score*10))), duration: 5)
+		let enemyNode = ObjectNode()
+		let x = position == nil ? Int.random(in: -1...1) : position!
+		enemyNode.geometry = box
+		enemyNode.position = SCNVector3(x*20, 0, -1000+(Int.random(in: -3...3)*20))
+		enemyNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: enemyNode))
+		enemyNode.physicsBody?.categoryBitMask = PhysicsObject.enemy.rawValue
+		enemyNode.physicsBody?.contactTestBitMask = PhysicsObject.contact.rawValue
+		enemyNode.physicsBody?.isAffectedByGravity = false
+		enemyNode.name = "enemy"
+		enemyNode.runAction(move, completionHandler: { enemyNode.destroy() })
+		scene.rootNode.addChildNode(enemyNode)
+		var delay = 5.0 + Double.random(in: -1...1) - Double(score/5)
+		if delay < 2 { delay = Double.random(in: 1...2) }
+		DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+			self?.createObject(nil)
+		}
+	}
+	
+	func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+		score += 1
+		if contact.nodeA.name != "player" {
+			if contact.nodeA.name == "enemy" { (contact.nodeA as? ObjectNode)?.destroy() }
+			else { contact.nodeA.removeFromParentNode() }
+		}
+		if contact.nodeB.name != "player" {
+			if contact.nodeB.name == "enemy" { (contact.nodeB as? ObjectNode)?.destroy() }
+			else { contact.nodeB.removeFromParentNode() }
+		}
 	}
 
-	func createBullet() {
+	@objc func createLaser() {
 		let plane = SCNBox(width: 1, height: 0.1, length: 10, chamferRadius: 0)
-		let move = SCNAction.move(by: SCNVector3(0, 0, 0), duration: 0.25)
+		let move = SCNAction.move(by: SCNVector3(0, 0, 0), duration: 0.5)
 		let material = SCNMaterial()
 		material.diffuse.contents = UIImage(named: Bundle.main.path(forResource: "laser", ofType: "jpg")!)
 		plane.materials = [material]
@@ -115,35 +143,12 @@ class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsCont
 		laserNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: laserNode))
 		laserNode.physicsBody?.categoryBitMask = PhysicsObject.laser.rawValue
 		laserNode.physicsBody?.collisionBitMask = PhysicsObject.asteroid.rawValue | PhysicsObject.enemy.rawValue
-		laserNode.physicsBody?.contactTestBitMask = PhysicsObject.asteroid.rawValue | PhysicsObject.enemy.rawValue
+		laserNode.physicsBody?.contactTestBitMask = PhysicsObject.contact.rawValue
 		laserNode.physicsBody?.isAffectedByGravity = false
-		laserNode.physicsBody?.applyForce(SCNVector3(0, 0, -250), asImpulse: true)
-		laserNode.position = SCNVector3(playerNode.position.x, playerNode.position.y-2, playerNode.position.z+5)
+		laserNode.physicsBody?.applyForce(SCNVector3(0, 0, -300), asImpulse: true)
+		laserNode.position = SCNVector3(playerNode.position.x, playerNode.position.y-0.1, playerNode.position.z+5)
 		laserNode.name = "laser"
 		laserNode.runAction(move, completionHandler: { laserNode.removeFromParentNode() })
-	}
-	
-	func collisionBetween(objA: SCNNode, objB: SCNNode) {
-		if objA.name == "player" {
-			health -= 1;
-			updateHealth()
-			objB.removeFromParentNode()
-		}
-		if objA.name == "laser" {
-			if objB.name == "enemy" {
-				// Check health of enemy
-			}
-			else if objB.name == "asteroid" {
-				score += 1;
-				updateScore()
-				objB.removeFromParentNode()
-			}
-			objA.removeFromParentNode()
-		}
-	}
-
-	@objc func tap() {
-		createBullet()
 	}
 
 	@objc func swipe(gesture: UIGestureRecognizer) {
@@ -151,14 +156,14 @@ class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsCont
 			switch swipeGesture.direction {
 				case UISwipeGestureRecognizer.Direction.right:
 					if playerLocation < 2 {
-						let move = SCNAction.move(by: SCNVector3(15, 0, 0), duration: 0.1)
+						let move = SCNAction.move(by: SCNVector3(20, 0, 0), duration: 0.1)
 						playerNode.runAction(move)
 						thrusterNode.runAction(move)
 						playerLocation += 1
 					}
 				case UISwipeGestureRecognizer.Direction.left:
 					if playerLocation > 0 {
-						let move = SCNAction.move(by: SCNVector3(-15, 0, 0), duration: 0.1)
+						let move = SCNAction.move(by: SCNVector3(-20, 0, 0), duration: 0.1)
 						playerNode.runAction(move)
 						thrusterNode.runAction(move)
 						playerLocation -= 1
@@ -171,9 +176,21 @@ class ViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsCont
 	
 }
 
+func updateScore() {
+	DispatchQueue.main.async { scoreLabel.text = "Score: \(score)" }
+}
+
+class ObjectNode: SCNNode {
+	func destroy() {
+		updateScore()
+		removeFromParentNode()
+	}
+}
+
 enum PhysicsObject: Int {
-	case player = 1
-	case asteroid = 2
-	case enemy = 4
-	case laser = 8
+	case contact = 1
+	case player = 2
+	case asteroid = 4
+	case enemy = 8
+	case laser = 16
 }
